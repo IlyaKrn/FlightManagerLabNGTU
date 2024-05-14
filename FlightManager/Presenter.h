6fee6ac9f89e93b61786165cef51f59e257dd361
lizpar9990 <145855865+lizpar9990@users.noreply.github.com>
2024-05-14 19:20:33 +0300

    Create Presenter.h

---
+++ FlightManager/Presenter.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include "RouteManager.h"
+
+
+#include "BaseFileRepository.h"
+#include "BaseRepository.h"
+#include "ExecutingRouteRepository.h"
+#include "PlaneRepository.h"
+#include "RouteRepository.h"
